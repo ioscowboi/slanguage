e5550c7a9a13a74988a39afab9c4ddfcef76b92e
CharDale Barnes <differentaccounts@hotmail.com>
2015-02-19 16:29:59 -0600

    Added the following features: -test number of correct/incorrect responses -added timer -added route handlers for win/loss scenario -added tests for resetting game variables

--- main.rb
+++ main.rb
@@ -18,13 +18,22 @@ require_relative 'Helper'
 
 
 random_number = 10
+game_count = 0
+lost = 0
+been_used = []
+
 get "/" do
   erb :page1, :layout => :startpageboilerplate #find the erb file in views/welcome.erb and return it
 end
 
-
 get "/begin" do
+  if game_count == 5
+    redirect to ("/winner")
+  end
   @id_picker = rand(1..random_number).to_i
+  while been_used.include? @id_picker
+    @id_picker = rand(1..random_number).to_i
+  end
   @image_picker = Card.grab_card(@id_picker)
   @right_choice = Answer.correct(@id_picker)
   @wrong_choices = Answer.bad_choice_generator(@id_picker)
@@ -37,23 +46,56 @@ get "/begin" do
   end
   @all_choices.shuffle!
   erb :start_game, :layout => :boilerplate #find the erb file in views/welcome.erb and return it
+
+end
+
+get "/validate" do
+  @their_answer = params["answer"]
+  
+  if @their_answer.to_i == 1
+    game_count += 1
+    if game_count >= 5
+      redirect to ("/winner")
+    end
+    redirect to ("/correct")
+  else
+    lost += 1
+    if lost >= 3
+      redirect to ("/game_over")
+    end
+    redirect to ("/try_again")
+  end
+  
+  erb :layout => :delay_boiler
 end
 
 get "/correct" do
-  erb :correct, :layout => :boilerplate
+  erb :correct, :layout => :delay_boiler
 end
 
 get "/try_again" do
-  erb :try_again, :layout => :boilerplate
+  erb :try_again, :layout => :delay_boiler
 end
 
+get "/winner" do
+  game_count = 0
+  been_used = []
+  lost = 0
+erb :winner, :layout => :winner_boiler
+end
 
-after "/begin" do
-  @their_answer = params["answer"]
-  if @right_choice.detect{:id}.id == @their_answer
-    redirect to ("/correct")
-  elsif @right_choice.detect{:id}.id != @their_answer
-    redirect to ("/try_again")
-  else
-  end
+get "/game_over" do
+  lost = 0
+  game_count = 0
+  been_used = []
+  
+erb :game_over, :layout => :game_over_boiler
+end
+
+get "/times_up" do
+  lost = 0
+  game_count = 0
+  been_used = []
+  
+erb :times_up, :layout => :game_over_boiler
 end
