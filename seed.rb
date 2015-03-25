5e3f1c25ef4274372d4544777155df80c2b4d2ba
CharDale Barnes <differentaccounts@hotmail.com>
2015-03-25 10:06:29 -0500

    Heroku app is running. Need to create seed data for the database and things will be fully functional.

---
+++ seed.rb
@@ -0,0 +1,7 @@
+# This file contains Ruby code that creates records for the application to work from scratch.
+
+# Example:
+#
+# card1 = Card.create({:stuff => "Stuff"})
+# card1.answers.create({...}) or Answer.create({:card_id => card1.id})
+
